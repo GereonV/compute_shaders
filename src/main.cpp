f8588e626e897e6be86492503a7f32beba92c3d4
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-04-07 19:00:17 +0200

    Use new compute shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <mutex>
+#include <random>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "managers.hpp"
@@ -11,6 +12,10 @@
 struct glfw_context {
 	~glfw_context() { glfwTerminate(); }
 };
+
+static struct {
+	float x, y, angle, _;
+} agents[1000];
 
 static class {
 public:
@@ -63,7 +68,7 @@ int main() {
 		ERROR_FROM_MAIN("glfwCreateWindow() failed\n");
 	framebuffer.init_unsynchronized(window);
 	glfwMakeContextCurrent(window);
-	glfwSwapInterval(0);
+	// glfwSwapInterval(0);
 	glfwSetFramebufferSizeCallback(window, [](GLFWwindow *, int width, int height) {
 		framebuffer.change(width, height);
 		glViewport(0, 0, width, height);
@@ -74,7 +79,10 @@ int main() {
 	auto render_program = builder.build(VERTEX_GLSL, FRAGMENT_GLSL);
 	if(builder.error())
 		ERROR_FROM_MAIN(builder.error_message());
-	auto compute_program = builder.build(COMPUTE_GLSL);
+	auto slime_program = builder.build(SLIME_GLSL);
+	if(builder.error())
+		ERROR_FROM_MAIN(builder.error_message());
+	auto postprocess_program = builder.build(POSTPROCESS_GLSL);
 	if(builder.error())
 		ERROR_FROM_MAIN(builder.error_message());
 	constexpr float vertices[]{
@@ -87,6 +95,14 @@ int main() {
 		0, 1, 2, // bottom right
 		0, 3, 2, // top left
 	};
+	std::mt19937 twister;
+	std::uniform_real_distribution<float> dist01;
+	std::uniform_real_distribution<float> angle_dist{0, 2 * 3.1415926535f};
+	for(auto & agent : agents) {
+		agent.x = dist01(twister);
+		agent.y = dist01(twister);
+		agent.angle = angle_dist(twister);
+	}
 	GLuint vao; glGenVertexArrays(1, &vao);
 	GLuint buffers[3]; auto & [vbo, ebo, ssbo] = buffers; glGenBuffers(3, buffers);
 	glBindVertexArray(vao);
@@ -96,32 +112,41 @@ int main() {
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glNamedBufferData(ebo, sizeof(indices), indices, GL_STATIC_DRAW);
-	// glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
-	// glNamedBufferData(ssbo, sizeof(float) * 4 * 1920 * 1080, nullptr, GL_DYNAMIC_COPY);
-	// float * shader_storage = new float[4 * 1920 * 1080];
-	// glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
+	glNamedBufferData(ssbo, sizeof(agents), agents, GL_DYNAMIC_COPY); // TODO rethink usage
+	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
 	auto [manager, width, height] = framebuffer.create_texture_manager();
-	auto compute = [&](GLsizei width, GLsizei height) {
-		manager.bind_to_image_unit(0, shader_image_access::write_only);
-		manager.bind_to_texture_unit(0);
-		compute_program.use_program();
-		glDispatchCompute(width, height, 1);
-		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
-		render_program.use_program();
-	};
-	compute(width, height);
+	manager.bind_to_image_unit(0, shader_image_access::read_write);
+	manager.bind_to_texture_unit(0);
+	double last_time{};
 	while(!glfwWindowShouldClose(window)) {
+		auto time = glfwGetTime();
+		auto delta_time = time - last_time;
 		glClear(GL_COLOR_BUFFER_BIT);
 		framebuffer.reset_and_call_if_changed(
-			[&](GLsizei width, GLsizei height) {
+			[&](GLsizei new_width, GLsizei new_height) {
+				width = new_width;
+				height = new_height;
 				manager.reset(width, height);
-				compute(width, height);
+				manager.bind_to_image_unit(0, shader_image_access::read_write);
+				manager.bind_to_texture_unit(0);
+				glNamedBufferSubData(ssbo, 0, sizeof(agents), agents);
 			}
 		);
-		// glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
-		// glGetNamedBufferSubData(ssbo, 0, sizeof(float) * 4 * 1920 * 1080, shader_storage);
+		slime_program.use_program();
+		glUniform1d(0, time);
+		glUniform1d(1, delta_time);
+		glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT | GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+		glDispatchCompute(sizeof(agents) / sizeof(*agents), 1, 1);
+		postprocess_program.use_program();
+		glUniform1d(0, delta_time);
+		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+		glDispatchCompute(width / 32 + 1, height / 32 + 1, 1);
+		render_program.use_program();
+		glMemoryBarrier(GL_TEXTURE_FETCH_BARRIER_BIT); // compute result
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, nullptr);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+		last_time = time;
 	}
 }
