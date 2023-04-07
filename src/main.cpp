0010607a12565741e9c268e081ad172b22853664
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-04-07 13:24:20 +0200

    Ensure thread-safety of modifications by FramebufferSizeCallback

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <mutex>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "managers.hpp"
@@ -11,8 +12,47 @@ struct glfw_context {
 	~glfw_context() { glfwTerminate(); }
 };
 
-static bool framebuffer_changed{};
-static GLsizei width, height;
+static class {
+public:
+	void init_unsynchronized(GLFWwindow * window) noexcept {
+		_changed = false;
+		glfwGetFramebufferSize(window, &_width, &_height);
+	}
+
+	void change(GLsizei width, GLsizei height) noexcept {
+		std::scoped_lock lock{_mutex};
+		_changed = true;
+		_width = width;
+		_height = height;
+	}
+
+	template<typename F>
+	void reset_and_call_if_changed(F && f) noexcept {
+		std::unique_lock lock{_mutex};
+		auto changed = _changed;
+		if(!changed)
+			return;
+		_changed = false;
+		lock.unlock();
+		static_cast<F &&>(f)(_width, _height);
+	}
+
+	[[nodiscard]] auto create_texture_manager() const noexcept {
+		struct result {
+			texture_manager manager;
+			GLsizei width, height;
+		};
+		_mutex.lock();
+		auto width = _width;
+		auto height = _height;
+		_mutex.unlock();
+		return result{{width, height}, width, height};
+	}
+private:
+	mutable std::mutex _mutex;
+	bool _changed;
+	GLsizei _width, _height;
+} framebuffer;
 
 int main() {
 	if(!glfwInit())
@@ -21,13 +61,11 @@ int main() {
 	auto window = glfwCreateWindow(1920, 1080, "Compute Shaders", nullptr, nullptr);
 	if(!window)
 		ERROR_FROM_MAIN("glfwCreateWindow() failed\n");
-	glfwGetFramebufferSize(window, &width, &height);
+	framebuffer.init_unsynchronized(window);
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(0);
 	glfwSetFramebufferSizeCallback(window, [](GLFWwindow *, int width, int height) {
-		framebuffer_changed = true;
-		::width = width;
-		::height = height;
+		framebuffer.change(width, height);
 		glViewport(0, 0, width, height);
 	});
 	if(!gladLoadGL())
@@ -62,21 +100,26 @@ int main() {
 	// glNamedBufferData(ssbo, sizeof(float) * 4 * 1920 * 1080, nullptr, GL_DYNAMIC_COPY);
 	// float * shader_storage = new float[4 * 1920 * 1080];
 	// glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
-	texture_manager manager{width, height};
-	while(!glfwWindowShouldClose(window)) {
-		glClear(GL_COLOR_BUFFER_BIT);
-		if(framebuffer_changed) {
-			framebuffer_changed = false;
-			manager.reset(width, height);
-		}
-		compute_program.use_program();
+	auto [manager, width, height] = framebuffer.create_texture_manager();
+	auto compute = [&](GLsizei width, GLsizei height) {
 		manager.bind_to_image_unit(0, shader_image_access::write_only);
+		manager.bind_to_texture_unit(0);
+		compute_program.use_program();
 		glDispatchCompute(width, height, 1);
-		// glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
-		// glGetNamedBufferSubData(ssbo, 0, sizeof(float) * 4 * 1920 * 1080, shader_storage);
 		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 		render_program.use_program();
-		manager.bind_to_texture_unit(0);
+	};
+	compute(width, height);
+	while(!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		framebuffer.reset_and_call_if_changed(
+			[&](GLsizei width, GLsizei height) {
+				manager.reset(width, height);
+				compute(width, height);
+			}
+		);
+		// glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+		// glGetNamedBufferSubData(ssbo, 0, sizeof(float) * 4 * 1920 * 1080, shader_storage);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, nullptr);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
